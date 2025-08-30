699670e5b23870a299a4310ed337e3dd338696f0
Pedro-Vargas26 <backupofpedros@gmail.com>
2025-08-30 16:52:45 -0700

    Update statistical.h

--- statistical.h
+++ statistical.h
@@ -7,14 +7,13 @@
 #define STATISTICAL_H
 class Statistical {
 private:
-	DynamicArray<double> dataset;
+    DynamicArray<double> dataset;
     double median(double arr[], int start, int end);
-public:
 
+public:
     Statistical(const DynamicArray<double>& dataset);
 
     /*Anthony's Portion*/
-        
     double minimum();
     double maximum();
     double range();
@@ -23,10 +22,9 @@ public:
     double median();
     double* mode(int& modeCount);
 
-
     /*Daisy's Portion*/
-    double varianceIn();
-    double standardDeviation();
+    double variance(bool sample = true) const;               // <-- new
+    double standardDeviation(bool sample = true) const;      // <-- new
     double midRange();
     double quartiles();
     double outliers();
@@ -34,13 +32,16 @@ public:
     double meanAbsoluteDeviation();
     double rootMeanSquare();
 
-
-
-
-    /*Pedros Portion*/
-
-
+    /*Pedro's Portion*/
+    double standardErrorOfMean(bool sample = true) const;    // <-- new
+    double skewness(bool sample = true) const;               // <-- adjust later
+    double kurtosis(bool sample = true) const;               // <-- adjust later
+    double kurtosisExcess(bool sample = true) const;
+    double coefficientVariation(bool sample = true) const;
+    double relativeStandardDeviation(bool sample = true) const;
+    //DynamicArray<pair<double, int>> frequencyTable()const;
 };
+
 
 #endif
 
