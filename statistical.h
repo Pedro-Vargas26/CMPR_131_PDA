6beabb1535adf547b0d6bfc9b8c8e2510dd3e41d
Anthony1418 <anthonyherrera2314@gmail.com>
2025-08-30 01:45:58 -0700

    Update Statistical.h

--- statistical.h
+++ statistical.h
@@ -2,22 +2,31 @@
 #include "DynamicArray.h"
 #include <vector>
 
-class statistical
+class Statistical
 {
+private:
+    DynamicArray<double> dataset;
 public:
-    double varianceIn(const double data[], int size);
-    double midrangeIn(const double data[], int size);
-    double quartilesIn(const double data[], int size, int q); // fixed typo
-    double quartilesIn(const double data[], int size);
-    double sumOfSquares(const double data[], int size);       // renamed for clarity
-    double outliers(const double data[], int size);
-    double findMinimum(const double data[], int size);
-    double findMaximum(const double data[], int size);
-    double findRange(const double data[], int size);
-    double findSum(const double data[], int size);
-    double findMean(const double data[], int size);
-    double findMedian(const double data[], int size);
-    std::vector<double> findMode(const double data[], int size);
+
+    Statistical(const DynamicArray<double>& dataset);
+    double mean() const;
+    double varianceIn();
+    double standarddeviation();
+    double midrangeIn();
+    double quartilesIn();
+    double outliers();
+    double thesumofsquares();
+    double median(double arr[], int start, int end);
+    double meanabsolutedeviation();
+    double rootmeansquare();
+
+    double findMinimum();
+    double findMaximum();
+    double findRange();
+    double findSum();
+    double findMean();
+    double findMedian();
+    double* findMode(int& modeCount);
 };
 
 // TO DO: bool validateDataSet(const DynamicArray<double>& data);
