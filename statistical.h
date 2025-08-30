1bd5a000efdf0d440d4a2f69ffae1d8c6f87977a
DayRamon <108428799+DayRamon@users.noreply.github.com>
2025-08-29 21:54:31 -0700

    Update statistical.h

--- statistical.h
+++ statistical.h
@@ -5,35 +5,24 @@
 #include "DynamicArray.h"
 
 class Statistical {
-  
+private:
+    DynamicArray<double> dataset; 
+ 
 public:
-    DynamicArray<double> dataset;
-    
     
     Statistical(const DynamicArray<double>& dataset);
+    double mean() const;
+    double varianceIn();
+    double standarddeviation();
+    double midrangeIn();
+    double quartilesIn();
+    double outliers();
+    double thesumofsquares();
+    double median(double arr[], int start, int end);
+    double meanabsolutedeviation();
+    double rootmeansquare();
 
 
-   static  double  standarddeviation(const DynamicArray<double>& dataset);
-
-    static double varianceIn(const DynamicArray<double>& dataset);
-
-    static double midrangeIn(const DynamicArray<double>& dataset);
-
-    static  double quartilesIn(const DynamicArray<double>& dataset);
-
-    static double  outliers(const DynamicArray<double>& dataset);
-
-    static  double mean(const DynamicArray<double>& data);
-
-    static double thesumofsquares(const DynamicArray<double>& dataset);
-
-    static double meanabsolutedeviation(const DynamicArray<double>& dataset); 
-
-   static double rootmeansquare(const DynamicArray<double>& dataset);
-
-private:
-
-    static double median(double arr[], int start, int end);
 };
   
 
