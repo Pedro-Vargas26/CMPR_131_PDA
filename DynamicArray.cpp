2cc6ddba2bad1c99065ac01d2464c6fac0d5c1a0
Pedro-Vargas26 <backupofpedros@gmail.com>
2025-08-30 23:17:51 -0700

    Update DynamicArray.cpp

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -58,11 +58,11 @@ void DynamicArray<T>::merge(T genericArr[], const int& beginningIndex, const int
 	}
 	delete[] leftArray;
 	delete[] rightArray;
-}
+	}
 
 template<typename T>
 void DynamicArray<T>::sort() {
-	if (p_size > 1)
+	if (p_size > 1) 
 		mergeSort(p_ptr, 0, p_size - 1);
 }
 
@@ -85,7 +85,7 @@ DynamicArray<T>::DynamicArray(const DynamicArray& rhs) {
 	if (rhs.p_size != 0 && rhs.p_ptr != nullptr) {
 		p_ptr = new T[rhs.p_size];
 		p_size = rhs.p_size;
-		for (int i = 0; i < rhs.p_size; i++) {
+		for (int i = 0; i < rhs.p_size;i++) {
 			p_ptr[i] = rhs.p_ptr[i];
 		}
 	}
@@ -96,16 +96,16 @@ DynamicArray<T>::DynamicArray(const DynamicArray& rhs) {
 }
 template<typename T>
 DynamicArray<T>::~DynamicArray() {
-	delete[] p_ptr;
-	p_ptr = nullptr;
-	p_size = 0;
+		delete[] p_ptr;
+		p_ptr = nullptr;
+		p_size = 0;
 }
 template <typename T>
 void DynamicArray<T>::append(const T& nVal) {
 	if (p_ptr != nullptr) {
 		T* n_ptr = nullptr;
 		n_ptr = new T[p_size + 1];
-		for (int i = 0; i < p_size; i++) {
+		for (int i = 0; i < p_size;i++) {
 			n_ptr[i] = p_ptr[i];
 		}
 		n_ptr[p_size] = nVal;
@@ -152,7 +152,7 @@ void DynamicArray<T>::remove(const int& sIndex) {
 		throw OutOfRange("ERROR: INVALID INDEX ATTEMPTED.");
 	}
 
-	if (p_size == 1) {
+	if (p_size == 1) { 
 		delete[] p_ptr;
 		p_ptr = nullptr;
 		p_size = 0;
@@ -172,9 +172,9 @@ void DynamicArray<T>::remove(const int& sIndex) {
 }
 
 template<typename T>
-bool DynamicArray<T>::exists(const T& needle) const {
+bool DynamicArray<T>::exists(const T& needle) const{
 	if (p_size == 0) return false;
-	for (int i = 0; i < p_size; i++) {
+	for (int i = 0; i < p_size;i++) {
 		if (needle == p_ptr[i])return true;
 	}
 	return false;
@@ -190,10 +190,22 @@ DynamicArray<T>& DynamicArray<T>::operator=(const DynamicArray& rhs) {
 		delete[] p_ptr;
 		p_size = rhs.p_size;
 		p_ptr = new T[rhs.p_size];
-		for (int i = 0; i < p_size; i++) {
+		for (int i = 0; i < p_size;i++) {
 			p_ptr[i] = rhs.p_ptr[i];
 		}
 	}
-	return *this;
+		return *this;
+	
+}
 
+
+
+template<typename T>
+const T* DynamicArray<T>::data() const  {
+	return p_ptr;
+}
+
+template <typename T>
+bool DynamicArray<T>::erase(const T& needle, bool allOrOne) {
+	return true;
 }
